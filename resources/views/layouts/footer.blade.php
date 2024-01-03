3372db9db5540cef9e3d4799bee7b6548a84e03c
euricodande12 <euricodande12@gmail.com>
2024-01-03 02:28:49 +0100

    Give it a try

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,34 @@
+<!--    *Footer start    -->
+<footer class="main-footer mt-5">
+        <div class="container-fluid h-100 w-100" id="img-main-footer">
+            <div class="d-flex align-items-center mt-5 h-100 row" id="div-1-footer">
+                <div class="align-items-center col-md-6 d-flex flex-column mt-5" id="div-1-1-footer">
+                    <img class="w-50" src="../img/footer-logo.png" alt="logo">
+                    <p class="fs-4 text-white w-50">Encontre seu lar, seu espaço, seu lugar!</p>
+                </div>
+                <div class="col-md-6 d-flex justify-content-center mt-5" id="div-1-2-footer">
+                    <div class="w-75">
+                        <h2 class="darkest-brown fs-1">Links utéis</h2>
+                        <ul class="mb-0 list-unstyled">
+                            <li>
+                                <a class="darkest-brown fs-4 nav-link" href="#">Sobre o U-MBANJI</a>
+                            </li>
+                            <li>
+                                <a class="darkest-brown fs-4 nav-link" href="#">Alugar/Comprar uma casa</a>
+                            </li>
+                            <li>
+                                <a class="darkest-brown fs-4 nav-link" href="#">Registrar sua propriedade no portal</a>
+                            </li>
+                            <li>
+                                <a class="darkest-brown fs-4 nav-link" href="#">Termos e Politicas de Privacidade</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-12 text-center text-white" id="copy-rights">
+                    <p>&copy; Copyright, todos os direitos reservados, 2024</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--    *End of footer    -->
