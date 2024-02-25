16d6339a1b582a574b09fa8244eae3b4d1d04e95
euricodande12 <euricodande12@gmail.com>
2024-02-25 11:53:17 +0200

    I guess so.

---
+++ resources/views/layouts_livewire/client-ownerH&F.blade.php
@@ -0,0 +1,81 @@
+<!--    *Header start    -->
+<header class="w-100">
+    <section class="align-items-center bg-other-cream-color d-flex justify-content-between pe-4 ps-4 py-2">
+        <div class="d-flex w-50">
+            <img alt="logo" class="w-25 " src="../img/footer-logo.png">
+        </div>
+        <div class="me-3">
+            <section class="bg-white d-flex align-items-center justify-content-evenly px-2 py-1 rounded-5">
+                <img alt="user" class="w-10-percent" src="../img/photo-round-mini.png">
+                <p class="m-0">Aceltino Kiaku Mateus Quianvo</p>
+                <i class="bi bi-caret-down-fill color-pink-orange"></i>
+            </select>
+        </div>
+    </section>
+
+    <!--    *Nav start  -->
+    <nav class="navbar navbar-expand-lg bg-white px-4">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Home</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="#">Alugar/Comprar casa</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Minhas propriedades</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Solicitações</a>
+                </li>
+            </ul>
+            <div class="align-items-center d-flex gap-2">
+                <a class="a-main-bg py-1 px-3 rounded-2 text-center text-white w-100" href="#">Registrar Casa</a>
+                <div class="bg-darkest-orange d-flex justify-content-center align-items-center  rounded-5" id="ico-bell-div">
+                    <i class="bi bi-bell-fill text-white"></i>
+                </div>
+            </div>
+            </div>
+        </div>
+    </nav>
+    <!--    *End of nav-->
+</header>
+<!--    *End of header    -->
+
+
+
+
+<!--    *Footer start    -->
+<footer class="main-footer">
+    <div class="container-fluid py-2 w-100" id="img-main-footer">
+        <div class="d-flex align-items-center mt-5 row" id="div-1-footer">
+            <div class="align-items-center col-md-6 d-flex flex-column mt-5" id="div-1-1-footer">
+                <img class="w-50" src="../img/footer-logo.png" alt="logo">
+                <p class="fs-4 text-white w-50">Encontre seu lar, seu espaço, seu lugar!</p>
+            </div>
+            <div class="col-md-6 d-flex justify-content-center mt-5" id="div-1-2-footer">
+                <div class="w-75">
+                    <h2 class="darkest-brown fs-1">Links utéis</h2>
+                    <ul class="mb-0 list-unstyled">
+                        <li>
+                            <a class="darkest-brown nav-link" href="#">Solicitar propriedade</a>
+                        </li>
+                        <li>
+                            <a class="darkest-brown nav-link" href="#">Registrar sua propriedade no portal</a>
+                        </li>
+                        <li>
+                            <a class="darkest-brown fw-bold  nav-link" href="#">Termos e Politicas de Privacidade</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 mt-5 text-center text-white" id="copy-rights">
+                <p>&copy; Copyright, todos os direitos reservados, 2024</p>
+            </div>
+        </div>
+    </div>
+</footer>
+    <!--    *End of footer    -->
